def8eff2795e554304d3ca27ad91941707901899
chajus1 <chajus1@googlemail.com>
2023-04-28 12:01:19 +0100

    Add project files.

---
+++ MiniProject2/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProject2
+{
+    internal class Car
+    {
+        public string Name;
+        public string Brand;
+        public int Millage;
+
+        public void DescriptionCar()
+        {
+            Console.WriteLine($"This is a {Brand} {Name} and it has {Millage} miles on it.\n");
+
+        }
+    }
+}
---
+++ MiniProject2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProject2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car() { Brand = "WolksWagen", Name = "Polo", Millage = 10_000};
+
+            car.DescriptionCar();
+        }
+    }
+}
